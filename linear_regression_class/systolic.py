b1b8e538d408da31e283a43061175c3ccaf2ec1d
Shane-Neeley <shane.neeley@gmail.com>
2018-06-26 16:29:19 -0700

    nm

--- linear_regression_class/systolic.py
+++ linear_regression_class/systolic.py
@@ -14,7 +14,6 @@ from builtins import range
 # sudo pip install -U future
 
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -24,12 +23,12 @@ X = df.as_matrix()
 
 # using age to predict systolic blood pressure
 plt.scatter(X[:,1], X[:,0])
-plt.show()
+# plt.show()
 # looks pretty linear!
 
 # using weight to predict systolic blood pressure
 plt.scatter(X[:,2], X[:,0])
-plt.show()
+# plt.show()
 # looks pretty linear!
 
 df['ones'] = 1
@@ -51,3 +50,42 @@ def get_r2(X, Y):
 print("r2 for x2 only:", get_r2(X2only, Y))
 print("r2 for x3 only:", get_r2(X3only, Y))
 print("r2 for both:", get_r2(X, Y))
+
+# built in scipy method produces the same answer for simple linear regression
+from scipy import stats
+y = [132, 143, 153, 162, 154, 168, 137, 149, 159, 128, 166]
+x = [52, 59, 67, 73, 64, 74, 54, 61, 65, 46, 72]
+x2 = [173, 184, 194, 211, 196, 220, 188, 188, 207, 167, 217]
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("r2 for x2 only:", r_value**2)
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x2,y)
+print("r2 for x3 only:", r_value**2)
+
+# multiple linear regression
+from sklearn import linear_model
+texts = np.array([y,x,x2]).T
+clf = linear_model.LinearRegression()
+clf.fit([t for t in texts],
+    [t for t in texts])
+# print(clf.coef_)
+
+# Other way to do it from stackoverflow: https://stackoverflow.com/questions/11479064/multiple-linear-regression-in-python
+import statsmodels.api as sm
+
+y = y
+
+x = [
+     x,x2
+     ]
+
+def reg_m(y, x):
+    ones = np.ones(len(x[0]))
+    X = sm.add_constant(np.column_stack((x[0], ones)))
+    for ele in x[1:]:
+        X = sm.add_constant(np.column_stack((ele, X)))
+    results = sm.OLS(y, X).fit()
+    return results
+
+print("r2 for both:", reg_m(y, x).summary())
