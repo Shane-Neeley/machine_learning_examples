5a454ebff0c09c477dc49542acdda34567642684
Shane-Neeley <shane.neeley@gmail.com>
2018-09-13 14:33:30 -0700

    stuff

--- shane/linearregression_tf.py
+++ shane/linearregression_tf.py
@@ -7,9 +7,9 @@ Project: https://github.com/aymericdamien/TensorFlow-Examples/
 from __future__ import print_function
 
 import tensorflow as tf
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-rng = numpy.random
+
 
 # Parameters
 learning_rate = 0.01
@@ -17,10 +17,11 @@ training_epochs = 1000
 display_step = 50
 
 # Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
+train_X = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
                          7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+train_Y = np.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
                          2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+
 n_samples = train_X.shape[0]
 
 # tf Graph Input
@@ -28,8 +29,8 @@ X = tf.placeholder("float")
 Y = tf.placeholder("float")
 
 # Set model weights
-W = tf.Variable(rng.randn(), name="weight")
-b = tf.Variable(rng.randn(), name="bias")
+W = tf.Variable(np.random.randn(), name="weight")
+b = tf.Variable(np.random.randn(), name="bias")
 
 # Construct a linear model mx+b
 pred = tf.add(tf.multiply(X, W), b)
@@ -71,8 +72,8 @@ with tf.Session() as sess:
     plt.show()
 
     # Testing example, as requested (Issue #2)
-    test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
-    test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
+    test_X = np.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
+    test_Y = np.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
 
     print("Testing... (Mean square loss Comparison)")
     testing_cost = sess.run(
